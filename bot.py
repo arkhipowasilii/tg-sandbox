fa36e54a6460e1dab4b6f18d02a314d60175a981
vas <rkhipowasilii@gmail.com>
2019-12-07 10:46:20 +0300

    lesson commit

--- bot.py
+++ bot.py
@@ -1,78 +1,61 @@
-from abc import abstractmethod
-from collections import Callable
-from enum import Enum
+from abc import abstractmethod, abstractproperty, abstractclassmethod
+from collections.abc import Callable
 from typing import Dict
-
 from telegram import Update
 from telegram.ext import Updater, MessageHandler, CommandHandler
-
 from core import Core
 from messagefilters import MessageFilters
+from enum import Enum
 
 
 # Принцип единичной ответственности
 
+
 class AbsModeHandler:
+    def __init__(self, dispatcher, core: Core):
+        self._core = core
+        self.dispatcher = dispatcher
+        self.current_handlers = []
+
+    def connect_handlers(self):
+        self.add_message_handlers()
+
+    @classmethod
     @abstractmethod
-    def connect_handlers(self, dispatcher):
-        raise NotImplementedError
+    def command(cls):
+        pass
 
     @abstractmethod
-    def disconnect_handlers(self, dispatcher):
-        raise NotImplementedError
+    def _get_message_handlers(self):
+        pass
+
+    def _get_handlers(self):
+        return self._get_message_handlers()
+
+    def disconnect_handlers(self):
+        for handler in self.current_handlers:
+            self.dispatcher.remove_handler(handler)
+
+    @staticmethod
+    def send_message(update: Update, context, msg: str):
+        context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
+
+    def add_message_handlers(self):
+        for filter, callback in self._get_message_handlers():
+            handler = MessageHandler(filter, callback)
+            self.current_handlers.append(handler)
+            self.dispatcher.add_handler(handler)
 
 
 class AnagramMode(AbsModeHandler):
-    pass
 
+    @classmethod
+    def command(cls):
+        return "anagram"
 
-class PalindromMode(AbsModeHandler):
-    pass
-
-
-class Modes(Enum):
-    Anagram = "anagram"
-    Palindrome = "palindrome"
-
-    @property
-    def command(self):
-        return self.value
-
-    @property
-    def handler(self):
-        if self.Anagram:
-            return AnagramMode
-        if self.Palindrome:
-            return PalindromMode
-
-
-class Bot:
-    def __init__(self, token: str, core: Core, mode: Modes):
-        self._core = core
-        self._mode = mode
-        self._modes = {mode: mode.handler() for mode in Modes}
-
-        self.updater = Updater(token=token, use_context=True)
-        if self._mode is Modes.Anagram:
-            self.dispatcher.add_handler(MessageHandler(MessageFilters.text, self.message_callback))
-            self.dispatcher.add_handler(MessageHandler(MessageFilters.add, self.message_add_callback))
-
-        self.add_command_handlers({"start": self.start_callback,
-                                   Modes.Anagram.command: self.start_callback,
-                                   Modes.Palindrome.command: self.palindrome_callback})
-
-    @property
-    def dispatcher(self):
-        return self.updater.dispatcher
-
-    def add_command_handlers(self, callbacks: Dict[str, Callable]):
-        tuple(self.dispatcher.add_handler(CommandHandler(command, func)) for command, func in callbacks.items())
-
-    def palindrome_callback(self, update, context):
-        self._mode = Modes.Palindrome
-
-    def start_callback(self, update: Update, context):
-        self.send_message(update, context, 'bot started')
+    def _get_message_handlers(self):
+        return ((MessageFilters.text, self.message_callback),
+                (MessageFilters.add, self.message_add_callback))
 
     def message_callback(self, update: Update, context):
         result = self._core.find_anagram(update.message.text)
@@ -91,9 +74,83 @@ class Bot:
 
         self.send_message(update, context, f"words: {words} successfully added")
 
-    def start_polling(self):
-        self.updater.start_polling()
+
+class PalindromeMode(AbsModeHandler):
+    @classmethod
+    def command(cls):
+        return "palindrome"
+
+    def _get_message_handlers(self):
+        return (MessageFilters.text, self.palindrome_message_callback),
+
+    def palindrome_message_callback(self, update: Update, context):
+        string = update.message.text
+        msg = f"{string} is a palindrome" if self._core.check_palindrome(
+            string) else f"{string} is NOT a palindrome"
+        self.send_message(update, context, msg)
+
+
+class Modes(Enum):
+    Anagram = AnagramMode
+    Palindrome = PalindromeMode
+
+    @property
+    def command(self) -> str:
+        return self.value.command()
+
+    @property
+    def handler(self):
+        return self.value
+
+
+class Bot:
+    default_mode = Modes.Palindrome
+
+    def __init__(self, token: str, core: Core, mode: Modes = None):
+        self._token = token
+        self._core = core
+        self._mode = mode or self.default_mode
+
+        self._updater = Updater(token=token, use_context=True)
+        self._disp = self._updater.dispatcher
+
+        self._add_command_handlers({"start": self.start_callback,
+                                    Modes.Palindrome.command:
+                                       lambda upt, ctx: self._switch_to_mode_callback(upt, ctx, Modes.Palindrome),
+                                    Modes.Anagram.command:
+                                       lambda upt, ctx: self._switch_to_mode_callback(upt, ctx, Modes.Anagram)})
+        self._handlers = {}
+        self._switch_to_mode(self.default_mode)
+
+    @property
+    def dispatcher(self):
+        return self._updater.dispatcher
+
+    def _add_command_handlers(self, callbacks: Dict[str, Callable]):
+        tuple(self.dispatcher.add_handler(CommandHandler(command, func)) for command, func in callbacks.items())
+
+    def start_callback(self, update: Update, context):
+        update.effective_chat.id
+        self.send_message(update, context, 'bot started')
+
+    def _switch_to_mode(self, mode):
+        if self._mode in self._handlers:
+            self._handlers[self._mode].disconnect_handlers()
+
+        self._mode = mode
+
+        if self._mode not in self._handlers:
+            self._handlers[self._mode] = self._mode.handler(self._disp, self._core)
+
+        self._handlers[self._mode].connect_handlers()
+
+    def _switch_to_mode_callback(self, update: Update, context, mode):
+        self._switch_to_mode(mode)
+        self.send_message(update, context, f"{self._mode.name} has been activated!")
 
     @staticmethod
     def send_message(update: Update, context, msg: str):
         context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
+
+    def start_polling(self):
+        self._updater.start_polling()
--- core.py
+++ core.py
@@ -1,22 +1,35 @@
 from typing import Optional, List
+from multiset import Multiset
 
-
-# ToDo Set `set` too `multiset`
 
 class Core:
     def __init__(self, dictionary: tuple):
-        self._dict = {word: set(word) for word in dictionary}
+        self._dict = {word: Multiset(word) for word in dictionary}
 
     def find_anagram(self, string: str) -> Optional[str]:
-        string = set(string)  # n*log(n)
+        string = Multiset(string)
 
-        for word, letters in self._dict:  # m
+        for word, letters in self._dict.items():
             if len(word) != len(string):
                 continue
 
-            if len(string) == len(letters.intersection(string)):  # O(min(l), min(r))
+            if len(string) == len(letters.intersection(string)):
                 return word
 
-    def update_dict(self, new_words: List[str]) -> int:
+    def update_dict(self, new_words: List[str]):
         for word in new_words:
-            self._dict[word] = set(word)
+            self._dict[word] = Multiset(word)
+
+    @staticmethod
+    def check_palindrome(string: str) -> bool:
+        if string == string[::-1]:
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    words = ("asd", "qwe")
+    core = Core(words)
+    print(core.find_anagram("weq"))
+    print(core.check_palindrome("tvvt"))
